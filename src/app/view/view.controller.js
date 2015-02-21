6334001584a455d6e35a3745edfaac2cf3e3a78c
LabsterX <labsterx@gmail.com>
2015-02-21 16:45:53 +0800

    linked diagram and table; added modal to show sequences; add error handling.

--- src/app/view/view.controller.js
+++ src/app/view/view.controller.js
@@ -8,9 +8,8 @@ angular.module('DNAViewer')
 			$scope.dna = null;
 			$scope.id = $routeParams.id;
 			$scope.error = null;
-			$scope.selectedFeatureMarker = null;
 			$scope.selectedFeature = null;
-			$scope.closePopup = false;
+			$scope.showDNAModal = false;
 
 			// Retrieve the DNA information by calling the DNAViewer Service with the DNA ID
 			$scope.getDNA = function() {
@@ -37,21 +36,68 @@ angular.module('DNAViewer')
 				}
 			}
 
+			// Select a feature
+			$scope.selectFeature = function (feature) {
+				$scope.selectedFeature = feature;
+			}
+
+			// Unselect a feature
+			$scope.unselectFeature = function (feature) {
+				$scope.selectedFeature = false;
+			}
+
+			// Check whether a feature is currenly selected
+			$scope.isFeatureSelected = function (feature) {
+				if ($scope.selectedFeature && $scope.selectedFeature.dnafeatureId == feature.dnafeatureId) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+
+			// Toggle feature selection
+			$scope.toggleFeatureSelection = function (feature) {
+				if ($scope.isFeatureSelected(feature)) {
+					$scope.unselectFeature(feature);
+				}
+				else {
+					$scope.selectFeature(feature);
+				}
+			}
+
 			// When user clicks on a feature in the DNA diagram, update corresponding variables
-			$scope.featureDiagramClick = function(event, feature, marker) {
-				$scope.selectedFeature = feature;
-				$scope.selectedFeatureMarker = marker;
-				$scope.closePopup = false;
+			$scope.featureDiagramClick = function(feature) {
+				// $scope.selectedFeature = feature;
+				$scope.toggleFeatureSelection(feature);
 				$scope.$apply();
 			};
 
+			// Show the DNA details modal
+			$scope.showModal = function () {
+				$scope.showDNAModal = true;
+			}
 
-			// When user clicks on a feature in the DNA table, update corresponding variables
-			$scope.featureTableClick = function(feature) {
-				$scope.selectedFeature = feature;
-				$scope.closePopup = false;
-				$scope.$apply();
-			};
+			// Hide the DNA details modal
+			$scope.hideModal = function () {
+				$scope.showDNAModal = false;
+			}
+
+			// To display very long DNA sequences, we need to wrap them into multiple lines
+			$scope.wrapDNASequence = function (sequence, lengthPerLine) {
+				if (!sequence) {
+					return '';
+				}
+				lengthPerLine = lengthPerLine ? lengthPerLine : 50;
+				var result = '';
+				for (var i = 0; i < sequence.length; i++) {
+					if (i > 0 && i % lengthPerLine == 0) {
+						result += "\n";
+					}
+					result += sequence[i];
+				}
+				return result;
+			}
 
 			$scope.getDNA();
 
